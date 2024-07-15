b110616d9c0158a1289215f45e6dd87014af0f43
artztobi <tobias.artz@student.uni-augsburg.de>
2024-07-15 15:13:14 +0200

    champs

---
+++ app/src/main/java/com/example/tftsimulator/TFT_System.kt
@@ -0,0 +1,100 @@
+package com.example.tftsimulator
+
+class TFT_System {
+
+    val cost1_champs = listOf<String>(
+        "Ahri",
+        "Caitlyn",
+        "Chogath",
+        "Darius",
+        "Garen",
+        "Jax",
+        "Khazix",
+        "Kobuko",
+        "KogMaw",
+        "Malphite",
+        "RekSai",
+        "Sivir",
+        "Yasuo"
+    )
+
+    val cost2_champs = listOf<String>(
+        "Aatrox",
+        "Gnar",
+        "Janna",
+        "Kindred",
+        "Lux",
+        "Neeko",
+        "Quiana",
+        "Riven",
+        "Senna",
+        "Shen",
+        "Teemo",
+        "Yorick",
+        "Zyra"
+    )
+
+    val cost3_champs = listOf<String>(
+        "Alune",
+        "Amumu",
+        "Aphelios",
+        "Bard",
+        "Diana",
+        "Illaoi",
+        "Soraka",
+        "Tahm Kench",
+        "Thresh",
+        "Tristana",
+        "Volibear",
+        "Yone",
+        "Zoe"
+    )
+
+    val cost4_champs = listOf<String>(
+        "Annie",
+        "Ashe",
+        "Galio",
+        "Kaisa",
+        "Kayn",
+        "Lee Sin",
+        "Lillia",
+        "Morgana",
+        "Nautilus",
+        "Ornn",
+        "Sylas",
+        "Syndra"
+    )
+
+    val cost5_champs = listOf<String>(
+        "Azir",
+        "Hwei",
+        "Irelia",
+        "Lissandra",
+        "Rakan",
+        "Sett",
+        "Udyr",
+        "Wukong",
+        "Xayah"
+    )
+
+    val pool_1_cost = 22
+    var pool_1_cost_chosen = 22
+
+    val pool_2_cost = 20
+    var pool_2_cost_chosen = 20
+
+    val pool_3_cost = 17
+    var pool_3_cost_chosen = 17
+
+    val pool_4_cost = 10
+    var pool_4_cost_chosen = 10
+
+    val pool_5_cost = 9
+    var pool_5_cost_chosen = 9
+
+    fun generate_pool_of_all_champs(pool:Int, name:String){
+
+
+    }
+
+}
